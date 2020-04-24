2e63dbbd8b0d00cb01b0df6cb60f4022d86f9e9e
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2020-04-24 18:49:52 +0300

    Added styles to login screen

--- app/src/main/java/com/mikhailovskii/timesapp/start/StartActivity.kt
+++ app/src/main/java/com/mikhailovskii/timesapp/start/StartActivity.kt
@@ -1,14 +1,22 @@
 package com.mikhailovskii.timesapp.start
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mikhailovskii.timesapp.R
+import com.mikhailovskii.timesapp.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_start.*
 
 class StartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        log_in_button.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
     }
 
 }
